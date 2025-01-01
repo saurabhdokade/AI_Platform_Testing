8e0f68d1dbf9cc0ddd7f5976e13d2675d6743575
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-01-01 19:18:17 +0530

    added

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,26 +3,26 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
-
-
 // const firebaseConfig = {
-//   apiKey: "AIzaSyBw4Ef_L1SGoQSNNBBqu2ma8izGMqQFJW4",
-//   authDomain: "e-commerce-696e8.firebaseapp.com",
-//   projectId: "e-commerce-696e8",
-//   storageBucket: "e-commerce-696e8.firebasestorage.app",
-//   messagingSenderId: "663292733395",
-//   appId: "1:663292733395:web:5222d02f8258cf408b7126",
-//   measurementId: "G-DF5D59C3KH"
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 // };
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBw4Ef_L1SGoQSNNBBqu2ma8izGMqQFJW4",
+  authDomain: "e-commerce-696e8.firebaseapp.com",
+  projectId: "e-commerce-696e8",
+  storageBucket: "e-commerce-696e8.firebasestorage.app",
+  messagingSenderId: "663292733395",
+  appId: "1:663292733395:web:5222d02f8258cf408b7126",
+  measurementId: "G-DF5D59C3KH"
+};
 
 
 const app = initializeApp(firebaseConfig);
